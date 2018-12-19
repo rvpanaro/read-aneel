72904d3c1964468a004f4435a2f38f2a15fd3ae3
RVP07 <renatovp@id.uff.br>
2018-12-19 01:05:27 -0200

    update diag.png with disc and cont variables box updt README.md

--- diag.R
+++ diag.R
@@ -5,11 +5,14 @@ grViz("
 digraph boxes {
 # add node statements
 node[shape = box]
-Type[label = ' Tipo de Dado ']; Categorical[label = 'Categórico'];Nominal; Ordinal; Numerical[label = 'Numérico'];
+Type[label = ' Tipo de Variável ']; Categorical[label = ' Categórico '];
+Nominal; Ordinal; Numerical[label = ' Numérico ']; 
+Discrete[label = ' Discreto ']; Continuous[label = ' Contínuo '] 
 
 ## add edge statements
 edge[arrow = triangle]
-Type -> Categorical; Type -> Numerical;Categorical -> Nominal; Categorical -> Ordinal
+Type -> Categorical; Type -> Numerical;Categorical -> Nominal; 
+Categorical -> Ordinal; Numerical -> Continuous; Numerical -> Discrete 
 
 ## add a graph statement
 graph[nodestep = 1]
